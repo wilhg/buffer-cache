4f1523770e0e2f8bc661a434deb3c8489bee13b6
cuebyte <cuebyte@outlook.com>
2014-08-08 17:50:19 +0800

    add update & modify

--- example/index.js
+++ example/index.js
@@ -1,16 +1,33 @@
 var bc = require('../index')();
 
 bc.set('number', 233);
+console.log(bc.type('number'));
 console.log(bc.get('number'));
 
 bc.set('string', 'I am string');
-console.log(bc.get('string'));
+console.log(bc.type('string'));
+console.log("before update:", bc.get('string'));
+bc.update('string', 'I am String');
+console.log("after update:", bc.get('string'));
 
 bc.set('object', {a:1, b:2});
+console.log(bc.type('object'));
+console.log(bc.get('object'));
+
+bc.update('object', {b:3, c:4});
 console.log(bc.get('object'));
 
 bc.set('array', [{a:1, b:2}, {c:4}]);
+console.log(bc.type('array'));
+console.log(bc.get('array'));
+bc.modify('array', function (data) {
+  data[0]='0';
+});
 console.log(bc.get('array'));
 
 bc.delete('object');
 console.log(bc.get('object'));
+console.log(bc.type('object'));
+
+
+console.log(bc.get('undefinedKey'));
--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+function getType(data) {
+  if (data === undefined) {
+    return 'undefined'
+  } else if (data === null) {
+    return 'null'
+  } else {
+    return Object.prototype.toString.call(data).slice(8,-1).toLowerCase();
+  }
+}
+
 exports = module.exports = BufferCache;
 
 function BufferCache(encoding) {
   if(!(this instanceof BufferCache)) return new BufferCache;
-  
   this.obj = {};
   this.encoding = encoding || 'utf-8';
+}
+
+BufferCache.prototype.type = function (key) {
+  return getType(this.get(key));
 }
 
 BufferCache.prototype.set = function (key, data) {
@@ -16,6 +29,31 @@ BufferCache.prototype.get = function (key) {
   var buf = this.obj[key];
   if(buf === undefined) return undefined;
   return JSON.parse(buf);
+}
+
+BufferCache.prototype.update = function (key, input, callback) {
+  var data = this.get(key);
+  switch (getType(input)) {
+    case 'function':
+      throw new Error("Do not accept Function type");
+      break;
+    case 'object':
+      for (k in input) {
+        var v = input[k];
+        if(getType(v) !== "function") data[k] = v;
+      }
+      this.set(key, data);
+      break;
+    default:
+      this.set(key, input);
+  }
+}
+
+BufferCache.prototype.modify = function (key, callback) {
+  var result = this.get(key);
+  callback(result);
+  this.set(key, result);
+  return result;
 }
 
 BufferCache.prototype.delete = function (key) {
